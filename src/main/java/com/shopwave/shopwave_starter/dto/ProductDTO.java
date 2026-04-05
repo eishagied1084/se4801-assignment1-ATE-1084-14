fb1c4fbaf5e1390dab0b9ad7eed4eb05aafd46ae
eishagied1084 <167006026+eishagied1084@users.noreply.github.com>
2026-04-05 04:00:33 -0700

    Add ProductDTO class for product data transfer

---
+++ src/main/java/com/shopwave/shopwave_starter/dto/ProductDTO.java
@@ -0,0 +1,22 @@
+// student ID: ATE/1084/14
+package com.shopwave.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int stock;
+    private Long categoryId;
+}
