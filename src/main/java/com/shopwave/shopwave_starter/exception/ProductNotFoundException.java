1626979fcebb588534bbeb70d9e32bd185153b70
eishagied1084 <167006026+eishagied1084@users.noreply.github.com>
2026-04-05 04:03:13 -0700

    Create ProductNotFoundException.java

---
+++ src/main/java/com/shopwave/shopwave_starter/exception/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+// student ID: ATE/1084/14
+package com.shopwave.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
